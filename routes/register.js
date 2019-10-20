638e310ce88e372137be6f39058584da4b4cd0a4
graemenickerson <graeme.nickerson@gmail.com>
2019-10-20 19:48:10 +0000

    Redirects to registration page and fails ad post request.

--- routes/register.js
+++ routes/register.js
@@ -1,10 +1,61 @@
 
 const express = require('express');
 const router  = express.Router();
+const bcrypt = require('bcrypt');
+const cookieSession = require('cookie-session');
+const bodyParser = require("body-parser");
 
+router.use(bodyParser.urlencoded({extended: true}));
+router.use(cookieSession({
+  name: 'session',
+  keys: [process.env.cookieString],
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 
-router.get("/", (req,res) => {
-    res.render("../views/register")
+module.exports = (db) => {
+
+  // Shows page for creating new user
+  router.get("/", (req,res) => {
+      res.render("../views/register")
+    });
+
+  const addUser = function(user) {
+    const values = [user.name, user.email, user.password];
+    const sqlStatment = `
+      INSERT INTO users (name, email, password)
+      VALUES ( $1, $2, $3)
+      RETURNING *;
+    `;
+    return db.query(sqlStatment, values)
+      .then(res => res.rows)
+      .catch((err) => {return null});
+  };
+
+  // Take new user info and stores it also assigns cookie
+  router.post('/', (req, res) => {
+
+    const user = req.body;
+    user.password = bcrypt.hashSync(user.password, 10);
+    console.log("here");
+
+    addUser(user)
+      .then(user => {
+        if (!user) {
+          res.send({error: "error"});
+          return;
+        }
+        console.log(user.id);
+
+        req.session.userId = user.id;
+        let templateVars = { user: user.id };
+        res.redirect('index', templateVars);
+      })
+      .catch(error => res.send(error));
   });
 
-  module.exports = router;
+
+  return router;
+};
+
+
+
--- server.js
+++ server.js
@@ -34,13 +34,14 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-// const widgetsRoutes = require("./routes/widgets");
+const registerRoutes = require("./routes/register");
+
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use('/map', require("./routes/map"));
-app.use('/register', require("./routes/register"))
+app.use('/register', registerRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
