1d9788d4c895186e74b3e163407ad9fd6987e630
caitlining <caitlincing@gmail.com>
2019-10-21 23:23:24 +0000

    Adding icons to maps in index page

--- db/seeds/02_map_icons.sql
+++ db/seeds/02_map_icons.sql
@@ -2,5 +2,5 @@
 INSERT INTO map_icons (icon, name)
 VALUES
 ('https://cdn4.iconfinder.com/data/icons/coffee-108/512/coffee-cafe-13-512.png', 'coffee'),
-('https://cdn4.iconfinder.com/data/icons/coffee-108/512/coffee-cafe-13-512.png', 'coffee2');
+('https://cdn4.iconfinder.com/data/icons/small-n-flat/24/star-512.png', 'star');
 
--- db/seeds/04_maps.sql
+++ db/seeds/04_maps.sql
@@ -2,6 +2,6 @@
 INSERT INTO maps (owner_id, icon_id, date_created, title, description) VALUES
 (1, 1, '2019-10-19', 'Cute map of Victoria!', 'This map shows all the places you should go !!'),
 (1, 1, '2019-07-03', 'Vegan restaurants in Vancouver', 'Safe places to eat for good vegans!'),
-(2, 1, '2019-09-22', 'Awesome map of NYC', 'The must-see spots of the big apple!'),
-(2, 1, '2019-09-23', 'Awesome map of Florence', 'The must-see spots of the not apple!');
+(2, 2, '2019-09-22', 'Awesome map of NYC', 'The must-see spots of the big apple!'),
+(2, 2, '2019-09-23', 'Awesome map of Florence', 'The must-see spots of the not apple!');
 
