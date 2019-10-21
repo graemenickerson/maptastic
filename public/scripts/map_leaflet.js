6037dee3c67ea5dcdb6411c21da0d5948d8ae0a7
emi-hi <emily.hillier@gmail.com>
2019-10-21 21:02:08 +0000

    seperated leaflet script into new file

---
+++ public/scripts/map_leaflet.js
@@ -0,0 +1,14 @@
+let map = L.map('mapid').setView([48.440200, -123.360000], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'mapbox.streets'
+}).addTo(map);
+
+map.on('click', function (e) {
+  console.log(e.latlng);
+  let marker = new L.marker(e.latlng).addTo(map);
+});
