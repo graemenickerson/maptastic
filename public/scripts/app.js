282ccd33264ffc474b72933cfe8fd1b264711d29
emi-hi <emily.hillier@gmail.com>
2019-10-21 20:52:35 +0000

    added popup stuff, added marker icons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,11 +10,18 @@ $(() => {
     }).done((points) => {
       let markers = []
       for(point of points.points) {
-        L.marker([point.lat, point.long]).addTo(map).bindPopup(`
+        const point_icon = L.icon({
+          iconUrl: point.img_loc,
+          iconSize:     [40, 45], // size of the icon
+          iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+          shadowAnchor: [4, 62],  // the same for the shadow
+          popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+        });
+
+        L.marker([point.lat, point.long], {icon: point_icon}).addTo(map).bindPopup(`
         <b>${point.title}</b> <br>
         ${point.description} <br>
         <img src=${point.picture} max width="150"  max height="150"> <br>
-        ${point.word}
         `);
         let markerL = [point.lat, point.long]
         markers.push(markerL)
