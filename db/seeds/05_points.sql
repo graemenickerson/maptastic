e87773c926f145d6c4596d24fe7a2985b406de93
emi-hi <emily.hillier@gmail.com>
2019-10-21 18:21:32 +0000

    added fitBounds to set the map to the extent of the points, and added some points to seeds to test it

--- db/seeds/05_points.sql
+++ db/seeds/05_points.sql
@@ -8,8 +8,11 @@ VALUES
 (1, 'A store', 'You can buy things here', 'https://comps.gograph.com/school-building-icon-simple-style_gg87512678.jpg', 48.432323, -123.358454, 1),
 (2, 'Chickpea', 'A NEAT PLACE', 'https://comps.gograph.com/school-building-icon-simple-style_gg87512678.jpg', 49.246667, -123.100959, 1),
 (3, 'NYC', 'The big apple', 'https://comps.gograph.com/school-building-icon-simple-style_gg87512678.jpg', 40.730610, -73.935242, 1),
-(4, 'Florencce', 'The big Florence', 'https://comps.gograph.com/school-building-icon-simple-style_gg87512678.jpg', 43.769562, 11.255814, 1);
-
-
-
-
+(4, 'Florencce', 'The big Florence', 'https://comps.gograph.com/school-building-icon-simple-style_gg87512678.jpg', 43.769562, 11.255814, 1),
+(4, 'Gelato', 'Probably some gelato here', 'https://www.phillymag.com/wp-content/uploads/sites/3/2019/06/best-gelato-philadelphia-gran-caffe-laquila.jpg', 43.78657927547492, 11.274533033635588, 1),
+(4, 'Gelato #2', 'Another gelato place', 'https://www.phillymag.com/wp-content/uploads/sites/3/2019/06/best-gelato-philadelphia-gran-caffe-laquila.jpg', 43.77793633132943, 11.284573311017164, 1),
+(4, 'Airport', 'The airport', 'https://upload.wikimedia.org/wikipedia/commons/5/59/Florence_-_Peretola_%28Amerigo_Vespucci%29_%28FLR_-_LIRQ%29_AN0591245.jpg', 43.80622396020984, 11.19856395149868,1),
+(4, 'museum', 'The museum', 'https://upload.wikimedia.org/wikipedia/commons/5/59/Florence_-_Peretola_%28Amerigo_Vespucci%29_%28FLR_-_LIRQ%29_AN0591245.jpg', 43.770387, 11.258254, 1),
+(4, 'Australia', 'Aussie, mate!', 'add a photo!', -29.280205150532606, 146.02240457651226, 1),
+(1, 'Cafe Fantastico', 'Good coffee', 'sadf', 48.43216516584064, -123.37835382740455, 1),
+(1, 'Cedar Hill Golf Course', 'Teddy likes to walk here!', 'will add photo later' , 48.4535641582925, -123.34557240070171, 1);
