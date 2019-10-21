0366f384ec91b14ff68a34e0a852a122a0746383
emi-hi <emily.hillier@gmail.com>
2019-10-21 21:07:03 +0000

    in on click function, check if marker exists and if it does, remove it before adding new

--- public/scripts/map_leaflet.js
+++ public/scripts/map_leaflet.js
@@ -8,7 +8,10 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
   id: 'mapbox.streets'
 }).addTo(map);
 
+let marker = {}
 map.on('click', function (e) {
-  console.log(e.latlng);
-  let marker = new L.marker(e.latlng).addTo(map);
+  if (marker !== undefined){
+    map.removeLayer(marker);
+  }
+  marker = new L.marker(e.latlng).addTo(map);
 });
