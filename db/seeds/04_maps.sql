c26431cb947b93f1825e1a3391e6efc7cf520ce8
caitlining <caitlincing@gmail.com>
2019-10-22 01:04:11 +0000

    Updating maps to make sense with icons

--- db/seeds/04_maps.sql
+++ db/seeds/04_maps.sql
@@ -1,10 +1,10 @@
 -- map table seeds here (Example)
 INSERT INTO maps (owner_id, icon_id, date_created, title, description) VALUES
-(1, 1, '2019-10-19', 'Cute map of Victoria!', 'This map shows all the places you should go !!'),
-(1, 2, '2019-07-03', 'Vegan restaurants in Vancouver', 'Safe places to eat for good vegans!'),
-(2, 3, '2019-09-22', 'Awesome map of NYC', 'The must-see spots of the big apple!'),
-(2, 4, '2019-09-23', 'Awesome map of Florence', 'The must-see spots of the not apple!'),
-(3, 5, '2019-09-23', 'Awesome map of Somewhere Else', 'The must-see spots of somewhere!'),
-(3, 6, '2019-09-23', 'Map', 'Things in a place!!'),
+(1, 5, '2019-10-19', 'Great Drinks in YYJ', 'This map shows all the places you should go !!'),
+(1, 6, '2019-07-03', 'Vegan restaurants in Vancouver', 'Safe places to eat for good vegans!'),
+(2, 3, '2019-09-22', 'Gossip Girl Filming Locations in NYC', 'The must-see spots of the big apple!'),
+(2, 2, '2019-09-23', 'Awesome map of Florence', 'The must-see spots of the not apple!'),
+(3, 4, '2019-09-23', 'Beautiful Parks Somewhere Else', 'The must-see spots of somewhere!'),
+(3, 1, '2019-09-23', 'Best Coffee Shops', 'Things in a place!!'),
 (1, 7, '2019-09-23', 'My Locals Things', 'Places I go. Come find me.');
 
