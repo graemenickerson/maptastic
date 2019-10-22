1122566bba0d992da6610d36bc7d8e02c15eba54
caitlining <caitlincing@gmail.com>
2019-10-22 17:04:43 +0000

    Update restaurant icon

--- db/seeds/02_map_icons.sql
+++ db/seeds/02_map_icons.sql
@@ -6,7 +6,7 @@ VALUES
 ('https://cdn3.iconfinder.com/data/icons/free-multimedia-line/128/multimedia-11-512.png', 'Film Locations'),
 ('https://cdn3.iconfinder.com/data/icons/happy-trees/100/tree-plant-forest-leaf-woods-512.png', 'Nature'),
 ('https://cdn4.iconfinder.com/data/icons/miu/24/food-drink-cocktail-wine-glass-outing-drink-outline-stroke-512.png', 'Cocktails'),
-('https://cdn4.iconfinder.com/data/icons/summer-holidays-line/128/food_meal_restaurant_summer_vacation_holidays-512.png', 'Restaurants'),
+('https://cdn0.iconfinder.com/data/icons/kitchen-utensil-set/128/Cloche_food_kitchen_restaurant_icon-512.png', 'Restaurants'),
 ('https://cdn3.iconfinder.com/data/icons/streamline-icon-set-free-pack/48/Streamline-18-512.png', 'My Neighborhood');
 
 
