6fa9909b0af8a47bdb3a0d1469a9e00c7550e17d
caitlining <caitlincing@gmail.com>
2019-10-23 22:10:55 +0000

    Update map icons to have different colour and also to be in the database as png files rather than referencing the icon website

--- db/seeds/02_map_icons.sql
+++ db/seeds/02_map_icons.sql
@@ -1,13 +1,13 @@
 -- map icons table seeds here (Example)
 INSERT INTO map_icons (icon, name)
 VALUES
-('https://cdn2.iconfinder.com/data/icons/barista/256/barista-icons_coffee-to-go-512.png', 'Coffee Shops'),
-('https://cdn1.iconfinder.com/data/icons/travel-line-good-life/512/passport-512.png', 'Travel Spots'),
-('https://cdn3.iconfinder.com/data/icons/free-multimedia-line/128/multimedia-11-512.png', 'Film Locations'),
-('https://cdn3.iconfinder.com/data/icons/happy-trees/100/tree-plant-forest-leaf-woods-512.png', 'Nature'),
-('https://cdn4.iconfinder.com/data/icons/miu/24/food-drink-cocktail-wine-glass-outing-drink-outline-stroke-512.png', 'Cocktails'),
-('https://cdn0.iconfinder.com/data/icons/kitchen-utensil-set/128/Cloche_food_kitchen_restaurant_icon-512.png', 'Restaurants'),
-('https://cdn3.iconfinder.com/data/icons/streamline-icon-set-free-pack/48/Streamline-18-512.png', 'My Neighborhood');
+('/images/map_icons/coffee.png', 'Coffee Shops'),
+('/images/map_icons/passport.png', 'Travel Spots'),
+('/images/map_icons/film.png', 'Film Locations'),
+('/images/map_icons/nature.png', 'Nature'),
+('/images/map_icons/drink.png', 'Cocktails'),
+('/images/map_icons/restaurant.png', 'Restaurants'),
+('/images/map_icons/house.png', 'My Neighborhood');
 
 
 
