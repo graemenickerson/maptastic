4def4041c8f0a2739fc4c9e1f34c8ea9c2bd46d6
caitlining <caitlincing@gmail.com>
2019-10-24 20:09:07 +0000

    Update hardcoded database

--- db/seeds/04_maps.sql
+++ db/seeds/04_maps.sql
@@ -1,7 +1,8 @@
 -- map table seeds here (Example)
 INSERT INTO maps (owner_id, icon_id, date_created, title, description, center_lat, center_long, zoom) VALUES
-(1, 2, '2019-10-19', 'Must See Spots of Florence', 'Everything you need to know for a trip to Florence', 43.7865792754749, 11.274533033635588, 14);
-
+(1, 2, '2019-10-19', 'Must See Spots of Florence', 'Everything you need to know for a trip to Florence', 43.7865792754749, 11.274533033635588, 14),
+(1, 3, '2018-10-19', 'Gossip Girl Filming Locations in NYC', 'Xoxo, Gossip Girl', 40.771457, -73.970213, 14),
+(2, 6, '2019-09-19', 'Vegan Restaurants in Vancouver', 'Delicious restaurants to try in Vancouver', 49.259798, -123.111889, 14);
 
 
 
