80770a70fb3b72a25a44e375af4d0bf7a951a908
caitlining <caitlincing@gmail.com>
2019-10-24 00:01:45 +0000

    Update styling and chickpea photo in database

--- db/seeds/05_points.sql
+++ db/seeds/05_points.sql
@@ -4,5 +4,5 @@
 INSERT INTO points (user_id, map_id, title, description, picture, lat, long, keyword_id, active)
 VALUES
 (1, 1, 'Gelato', 'Probably some gelato here', 'https://www.phillymag.com/wp-content/uploads/sites/3/2019/06/best-gelato-philadelphia-gran-caffe-laquila.jpg', 43.78657927547492, 11.274533033635588, 15, true),
-(2, 2, 'Chickpea', 'A NEAT PLACE', 'https://comps.gograph.com/school-building-icon-simple-style_gg87512678.jpg', 49.246667, -123.100959, 16, true),
+(2, 2, 'Chickpea', 'Delicious Mediterranean Food', 'http://vanmag.com/wp-content/uploads/2018/03/chickpea-2.jpg', 49.246667, -123.100959, 16, true),
 (3, 3, 'Central Park', 'So wonderful for every reason', 'https://media.timeout.com/images/103451632/630/472/image.jpg', 40.776272, -73.970489, 8, true);
