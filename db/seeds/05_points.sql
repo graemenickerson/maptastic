183152e5436d78846c7d713ab5b09e849a854329
caitlining <caitlincing@gmail.com>
2019-10-24 00:45:43 +0000

    Updating seed data for demo

--- db/seeds/05_points.sql
+++ db/seeds/05_points.sql
@@ -4,5 +4,8 @@
 INSERT INTO points (user_id, map_id, title, description, picture, lat, long, keyword_id, active)
 VALUES
 (1, 1, 'Gelato', 'Probably some gelato here', 'https://www.phillymag.com/wp-content/uploads/sites/3/2019/06/best-gelato-philadelphia-gran-caffe-laquila.jpg', 43.78657927547492, 11.274533033635588, 15, true),
-(2, 2, 'Chickpea', 'Delicious Mediterranean Food', 'http://vanmag.com/wp-content/uploads/2018/03/chickpea-2.jpg', 49.246667, -123.100959, 16, true),
-(3, 3, 'Central Park', 'So wonderful for every reason', 'https://media.timeout.com/images/103451632/630/472/image.jpg', 40.776272, -73.970489, 8, true);
+(1, 1, 'Ponte Vecchio', 'A beautiful bridge with great shops.', 'https://www.visittuscany.com/shared/visittuscany/immagini/localita/ponte-vecchio-from-uffizi.jpg?__scale=w:1500,h:1000,t:2,q:85', 43.7679840812645, 11.2531225732528, 5, true),
+(1, 1, 'Giardino di Boboli', 'Probably some gelato here', 'http://selectitaly.com/blog/wp-content/uploads/2017/02/florence-palazzo-pitti-1024x681.jpg', 43.7634110304952, 11.2501185620204, 5, true),
+(1, 1, 'Uffizi Gallery', 'Incredible art museum', 'https://uffizi-webpromotersncva.netdna-ssl.com/wp-content/uploads/2016/03/uffizi-piazzale.jpg', 43.7683641221825, 11.2556648254395, 17, true),
+(1, 1, 'Train Station', '', 'https://www.rometoolkit.com/Images/xflorence_santa_maria_station.jpg.pagespeed.ic.7Sv6HiqH8a.jpg', 43.7765148643839, 11.247639656066, 21, true),
+(1, 1, 'Duomo', 'The beautiful cathedral and bell tower', 'https://www.yogawinetravel.com/wp-content/uploads/2016/05/Duomo-in-Florence-Italy_feature.jpg', 43.7729990698826, 11.2557745655067, 9, true);
