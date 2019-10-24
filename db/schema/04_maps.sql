f1e7a6dd8241846a9027861c4912bf9443e9c149
caitlining <caitlincing@gmail.com>
2019-10-24 21:35:04 +0000

    Update map timestamp from date to timestamp

--- db/schema/04_maps.sql
+++ db/schema/04_maps.sql
@@ -4,7 +4,7 @@ CREATE TABLE maps (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   icon_id INTEGER REFERENCES map_icons(id) ON DELETE CASCADE,
-  date_created DATE,
+  date_created TIMESTAMP,
   title VARCHAR(100),
   description TEXT,
   center_lat DOUBLE PRECISION,
